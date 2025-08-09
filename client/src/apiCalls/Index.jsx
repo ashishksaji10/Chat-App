d42cca013bc5a4a016e801b6cccba08a19eb5729
ashishksaji10 <155794189+ashishksaji10@users.noreply.github.com>
2025-08-09 23:05:42 +0530

    adding deployed backend url

--- client/src/apiCalls/Index.jsx
+++ client/src/apiCalls/Index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = "http://localhost:5000";
+export const url = "https://chat-app-1-wz1x.onrender.com";
 
 export const axiosInstance = axios.create({
     headers: {
